eb7afb9d44ccec0de9adc0115f37d8bcebd0e10e
herloncosta <herlon36@gmail.com>
2024-09-12 21:13:34 -0300

    feat: implement rate limit

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 from routes.product_routes import products_router
 
 app = Flask(__name__)
 app.register_blueprint(products_router)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"]
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
