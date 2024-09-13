0e6b0f03ec70daf7369c67878f1ca20e5480618e
herloncosta <herlon36@gmail.com>
2024-09-12 21:46:31 -0300

    feat: create product schama

--- entities/product.py
+++ entities/product.py
@@ -1,3 +1,8 @@
+from dataclasses import dataclass
+from marshmallow import Schema, fields, validate, post_load
+
+
+@dataclass
 class Product:
     def __init__(self, id, name, description, price):
         self.id = id
@@ -7,3 +12,15 @@ class Product:
 
     def __repr__(self):
         return f"<Product(id={self.id}, name={self.name}, description={self.description}, price={self.price})>"
+
+
+class ProductSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True, validate=validate.Length(min=3, max=50))
+    description = fields.Str(
+        required=True, validate=validate.Length(min=10, max=100))
+    price = fields.Float(required=True, validate=validate.Range(min=0.01))
+
+    @post_load
+    def make_product(self, data, **kwargs):
+        return Product(**data)
